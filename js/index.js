8f4cfd346fb105d837691dbfb1026fc791a325de
PhillDev-coder256 <phillipphilcourts540@gmail.com>
2023-08-22 16:01:54 +0300

    Changes

---
+++ js/index.js
@@ -0,0 +1,10 @@
+
+// Hamburger menu toggle
+function toggleMenu() {
+  var i = document.getElementById('nav');
+  var list = document.getElementById('list')
+
+  i.style.display == 'block'?
+  (i.style.display = 'none') :
+  i.style.display = 'block';
+}
